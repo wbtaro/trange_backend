fbde85a9601affac7ee101f6cf1755926efe46a7
wbtaro <m.ryotaro.325@gmail.com>
2021-01-03 06:09:44 +0900

    Change "" to '' in test_incremental_search.py

--- tests/test_inceremental_search.py
+++ tests/test_inceremental_search.py
@@ -6,9 +6,9 @@ from source import incremental_search
 class IncrementalSearchTonditionTest(unittest.TestCase):
     def test_incremental_search(self):
         test_cases = [
-            [{'queryStringParameters': {"baseStationName": "相"}}, {"hasOneOrManyResults": True}],
-            [{'queryStringParameters': {"baseStationName": "aaa"}}, {"hasOneOrManyResults": False}],
-            [{'queryStringParameters': {"baseStationName": ""}}, {"hasOneOrManyResults": False}],
+            [{'queryStringParameters': {'baseStationName': '相'}}, {'hasOneOrManyResults': True}],
+            [{'queryStringParameters': {'baseStationName': 'aaa'}}, {'hasOneOrManyResults': False}],
+            [{'queryStringParameters': {'baseStationName': ''}}, {'hasOneOrManyResults': False}],
         ]
         for case, expected in test_cases:
             with self.subTest(case):
